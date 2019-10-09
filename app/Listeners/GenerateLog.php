d105df2303f60c6d9c2683a8b496e405aff70971
idstack <idstack@idstacks-MacBook-Pro.local>
2019-10-09 13:32:09 +0700

    generate event and listener

---
+++ app/Listeners/GenerateLog.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserLogged;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Storage;
+
+class GenerateLog
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserLogged  $event
+     * @return void
+     */
+    public function handle(UserLogged $event)
+    {
+        $message = $event->request->user()->name . "just {$event->type} from the application";
+        Storage::put('logactivity.txt', $message);
+    }
+}
